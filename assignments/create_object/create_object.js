065bfa917465e18af64adfb71a83f1f87ed69274
alphacodedemo <abhijeet.singh@alphacode.com>
2022-03-14 21:58:42 +0530

    third-js-assignment-submission

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,10 @@
 function CreateObject(arr) {
     // Write your code here
-}
+ const dic={};
+ for(let i=0;i<arr.length;i=i+2){
+     dic[arr[i]]=arr[i+1];
+ }
+ return dic;
+}  
 
 module.exports = CreateObject;
